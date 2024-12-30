d84ea54b8f96efd2e3d2647530d49bfe8a12ca3a
Ziad Omaish <ziadomaish@dimensions-studio.com>
2024-12-30 16:56:05 +0300

    create account dto

--- Application/DTOs/Request/Account/CreateAccountDTO.cs
+++ Application/DTOs/Request/Account/CreateAccountDTO.cs
@@ -1,6 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Application.DTOs.Request.Account;
 
-public class CreateAccountDTO
+public class CreateAccountDTOc : LoginDTO
 {
-    
+    [Required]
+    public String Name { get; set; } = string.Empty;
+    [Required, Compare(nameof(Password))]
+    public String ConfirmPassword { get; set; } = string.Empty;
+    [Required]
+    public String Role {get; set;} = string.Empty;
 }
