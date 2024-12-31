e5352b107aa8cd1bc581d343090e98391e931321
Ziad Omaish <ziadomaish@dimensions-studio.com>
2024-12-31 04:41:38 +0300

    adding CustomAutheticationStateProvider.cs

---
+++ Application/Extensions/CustomAutheticationStateProvider.cs
@@ -0,0 +1,76 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Application.DTOs.Request.Account;
+using Application.DTOs.Respose.Account;
+using Application.Extentions;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace Application.Extensions;
+
+public class CustomAuthenticationStateProvide(LocalStorageService localStorageService) : AuthenticationStateProvider
+{
+    private readonly ClaimsPrincipal anonymous = new(new ClaimsIdentity());
+
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        var tokenModel = await localStorageService.GetModelFromToken();
+        if (string.IsNullOrEmpty(tokenModel.Token))
+            return await Task.FromResult(new AuthenticationState(anonymous));
+
+        var getUserClaims = DecryptToken(tokenModel.Token!);
+        if (getUserClaims == null) return await Task.FromResult(new AuthenticationState(anonymous));
+
+        var claimsPrincipal = SetClaimsPrincipal(getUserClaims);
+        return await Task.FromResult(new AuthenticationState(claimsPrincipal));
+    }
+
+    public async Task UpdateAutheticationState(LocalStorageDto localStorageDto)
+    {
+        var claimsPrincipal = new ClaimsPrincipal();
+        if(localStorageDto.Token != null||localStorageDto.RefreshToken!=null)
+        {
+            await localStorageService.SetBrowserLocalStorage(localStorageDto);
+            var getUserClaims = DecryptToken(localStorageDto.Token!);
+            claimsPrincipal = SetClaimsPrincipal(getUserClaims);
+        }
+        else
+        {
+            await localStorageService.RemoveTokeFromBrowserLocalStorage();
+        }
+        NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(claimsPrincipal)));
+    }
+
+    public static ClaimsPrincipal SetClaimsPrincipal(UserClaimsDTO claims)
+    {
+        if (claims.Email is null) return new ClaimsPrincipal();
+        return new ClaimsPrincipal(new ClaimsIdentity(
+        [
+            new(ClaimTypes.Name, claims.UserName!),
+            new(ClaimTypes.Email, claims.Email!),
+            new(ClaimTypes.Role, claims.Role!),
+            new Claim
+                ("Fullname", claims.Fullname),
+        ], Constant.AuthenticationType));
+    }
+    
+    private static UserClaimsDTO DecryptToken(string jwtToken)
+    {
+        try
+        {
+            if (string.IsNullOrEmpty(jwtToken)) return new UserClaimsDTO();
+
+            var handler = new JwtSecurityTokenHandler();
+            var token=handler.ReadJwtToken(jwtToken);
+            
+            var name=token.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Name)!.Value;
+            var email=token.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Email)!.Value;
+            var role=token.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Role)!.Value;
+            var fullname=token.Claims.FirstOrDefault(x => x.Type == "Fullname")!.Value;
+            return new UserClaimsDTO(fullname,name,email,role);
+        }
+        catch
+        {
+            return null!;
+        }
+    }
+}
--- WebUI/obj/Debug/net8.0/WebUI.AssemblyInfo.cs
+++ WebUI/obj/Debug/net8.0/WebUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9a0ea6a758e7233825fb236f82c3840e3d1dc9b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ad1aceedcdef8c002e6e6af8e288adbfa90cfe2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
